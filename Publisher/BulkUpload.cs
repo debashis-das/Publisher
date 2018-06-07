a6ed172e4321cf13ae471af4242e3d6ef9d3d61b
debashd-ttltw <Debashis.Das@thetrainline.com>
2018-06-07 14:34:11 +0530

    Added console Publisher | <Debashis>

---
+++ Publisher/BulkUpload.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JustSaying.Models;
+
+namespace Publisher
+{
+    public class BulkUpload : Message
+    {
+        public BulkUpload(int orderInfo)
+        {
+            OrderInfo = orderInfo;
+        }
+        public int OrderInfo { get; private set; }
+    }
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Amazon;
+using Amazon.Runtime;
+using CorporateBulkWork.Registration.DTO;
+using JustSaying;
+using JustSaying.AwsTools;
+using TTL.ManagementInformationService.Client.Contract;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //need to give aws cred
+            var tempCred = new BasicAWSCredentials("", "");
+            CreateMeABus.DefaultClientFactory = () =>
+                new DefaultAwsClientFactory(tempCred);
+            var loggerFactory = new Log4NetProvider();
+
+            var publisher = CreateMeABus.WithLogging(loggerFactory)
+                 .InRegion(RegionEndpoint.EUWest1.SystemName)
+                 .WithSnsMessagePublisher<BusinessUserRegistrationDetails>();
+            for (int i = 0; i < 10; i++)
+            {
+                var bussinessData = new BusinessUserRegistrationDetails()
+                {
+                    MasterGuid = new Guid("312916C1-9168-E811-822F-CC3D82DF33E1"),
+                    CustomerId = 0,
+                    ManagedGroupId = "15",
+                    CorporateId = "91371",
+                    EntryPointId = "203",
+                    AffliatedId = "",
+                    CorporateReference = "TBDEMO1",
+                    EmailAddress = "demo"+i+"123@test.com",
+                    Password = "Password",
+                    Title = "MR",
+                    ForeName = "ForeName",
+                    Surname = "Surname",
+                    PhoneNumber = "123456789",
+                    AlternatePhoneNumber = "1234567",
+                    DataProtectionAct1984ConsentIndicator = "N",
+                    DataProtectionAct2003ConsentIndicator = "N",
+                    TermsAndConditionsConsentIndicator = true,
+                    EmailConfirmationRequired = "Yes",
+                    ContactAddressLine1 = "address1",
+                    ContactAddressLine2 = "address2",
+                    ContactAddressLine3 = "address",
+                    ContactAddressLine4 = "address4",
+                    ContactAddressLine5 = "address5",
+                    ContactPostCode = "SE17EF",
+                    ContactCountryCode = "Z0",
+                    ProfileAnswers = new List<SubmittedAnswer>()
+                    {
+                        new SubmittedAnswer() {QuestionText = "Profile Question 1", Text = "Q4*6319"},
+                        new SubmittedAnswer() {QuestionText = "Q9", Text = "9"}
+                    }
+
+                };
+                publisher.Publish(bussinessData);
+            }
+        }
+    }
+}
